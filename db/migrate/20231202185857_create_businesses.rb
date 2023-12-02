0659eb1dd41d20d44cdd17de52b7dafefe4892b1
MasudaAfrin <masudatuba@gmail.com>
2023-12-03 01:22:35 +0600

    fix permission error

--- db/migrate/20231202185857_create_businesses.rb
+++ db/migrate/20231202185857_create_businesses.rb
@@ -1,7 +1,8 @@
 class CreateBusinesses < ActiveRecord::Migration[7.0]
   def change
     create_table :businesses do |t|
-
+      t.string :name, null: false
+      t.integer :shares_available, null: false, default: 0
       t.timestamps
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -15,6 +15,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_12_02_185857) do
   enable_extension "plpgsql"
 
   create_table "businesses", force: :cascade do |t|
+    t.string "name", null: false
+    t.integer "shares_available", default: 0, null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
