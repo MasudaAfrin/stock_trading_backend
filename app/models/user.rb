3a36a2c6c2c9b71c20dcd870030f795f5509140d
MasudaAfrin <masudatuba@gmail.com>
2023-12-04 02:38:19 +0600

    business and user relation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,12 @@
 class User < ApplicationRecord
   has_secure_password validations: false
   has_secure_token :auth_token, length: 30
+
+  # Association
+  has_many :businesses
+
+  # validation
+  validates :name, :email, presence: true
+  validates :email, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP,
+                                               message: 'Please provide valid email' }
 end
