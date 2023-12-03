a7fe5d0f39b889156d1e8ba0fb7154441506b39e
MasudaAfrin <masudatuba@gmail.com>
2023-12-04 02:38:39 +0600

    user create in seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+user = User.find_or_create_by!(email: 'john@stock.com',
+                               name: 'John Doe')
+user.update!(password: 'pass123456@')
+p "User is created #{user.email}"
