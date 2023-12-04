0613aef01a00e650a63c1d49bcd0e23faff670de
MasudaAfrin <masudatuba@gmail.com>
2023-12-04 13:06:01 +0600

    stcok trade in schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_12_03_194417) do
+ActiveRecord::Schema[7.0].define(version: 2023_12_04_054603) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -23,6 +23,17 @@ ActiveRecord::Schema[7.0].define(version: 2023_12_03_194417) do
     t.index ["user_id"], name: "index_businesses_on_user_id"
   end
 
+  create_table "stock_trades", force: :cascade do |t|
+    t.decimal "price", precision: 10, scale: 2, default: "0.0"
+    t.integer "quantity", default: 1
+    t.integer "trade_status", default: 0
+    t.bigint "business_id", null: false
+    t.bigint "buyer_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["business_id"], name: "index_stock_trades_on_business_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "name", null: false
     t.string "email", null: false
@@ -33,4 +44,5 @@ ActiveRecord::Schema[7.0].define(version: 2023_12_03_194417) do
   end
 
   add_foreign_key "businesses", "users"
+  add_foreign_key "stock_trades", "businesses"
 end
