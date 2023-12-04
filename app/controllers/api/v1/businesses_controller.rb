e7ffd72550e4de88137f2bbc4757ad5b9b53e68f
MasudaAfrin <masudatuba@gmail.com>
2023-12-04 18:50:49 +0600

    business history api

--- app/controllers/api/v1/businesses_controller.rb
+++ app/controllers/api/v1/businesses_controller.rb
@@ -10,6 +10,16 @@ module Api
         render json: failed_response(e.message), status: :unprocessable_entity
       end
 
+      def show
+        business = Business.find(params[:id])
+        histories = business.stock_trades.accepted.order(id: :desc)
+        render json: success_response(histories, 'Successfully business shares fetched'),
+               status: :ok
+      rescue => e
+        Rails.logger.error("Business shares fetch failed due to #{e.full_message}")
+        render json: failed_response(e.message), status: :unprocessable_entity
+      end
+
       def create
         business = @current_user.businesses.create!(business_params)
         render json: success_response(business, 'Successfully business share created'),
